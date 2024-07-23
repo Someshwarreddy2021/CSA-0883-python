f15d0843b13049eda762173b97d8431a8fb1a25e
Someshwarreddy2021 <141160527+Someshwarreddy2021@users.noreply.github.com>
2024-07-23 08:24:02 +0530

    Add files via upload

---
+++ python for 17-07-2024 (3).py
@@ -0,0 +1,12 @@
+def reverse_array(arr):
+    return arr[::-1]
+
+
+array = [26, 28, 37, 26, 33, 31, 29]
+
+
+reversed_array = reverse_array(array)
+
+print("Original array:", array)
+print("Reversed array:", reversed_array)
+
