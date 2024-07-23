114ebd2184c8050ce3e7699db4a9249d0cd89c6a
Someshwarreddy2021 <141160527+Someshwarreddy2021@users.noreply.github.com>
2024-07-23 08:27:11 +0530

    Add files via upload

---
+++ two strings same or not in same index(18-07-24).py
@@ -0,0 +1,22 @@
+def count_matching_characters(s1, s2):
+    if len(s1) != len(s2):
+        return "Strings must be of the same length."
+
+    match_count = 0
+
+    for i in range(len(s1)):
+        if s1[i] == s2[i]:
+            match_count += 1
+
+    return match_count
+
+def main():
+    S1 = "what"
+    S2 = "watc"
+
+    matches = count_matching_characters(S1, S2)
+
+    print("Number of matching characters:", matches)
+
+if __name__ == "__main__":
+    main()
